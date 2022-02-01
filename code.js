2b1876ea264340407beeccbd188bde95a6d4de0f
Ikegami Daisuke <ikegami.da+gmail.com>
2022-02-01 17:12:18 +0900

    :beers: サンプルコードを試す get value
    https://www.w3schools.com/jquery/jquery_dom_get.asp

--- code.js
+++ code.js
@@ -1,19 +1,5 @@
-export function addElement () {
-    // 新しい div 要素を作成します
-    var newDiv = document.createElement("div");
-    // いくつかの内容を与えます
-    var newContent = document.createTextNode("Hi there and greetings!");
-    // テキストノードを新規作成した div に追加します
-    newDiv.appendChild(newContent);
-
-    // DOM に新しく作られた要素とその内容を追加します
-    var currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
-}
-
-export function getParityCheckMatrix () {
-    var currentDiv = document.getElementById('div1');
-    var raw = document.getElementById('parity_check_matrix');
-
-    document.body.insertBefore(raw.value, currentDiv);
-}
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        alert("Text: " + $("#test").text());
+    });
+});
